24d2cb5ff89929830031960dbb36dce6f8602a91
Grant Ramsay <seapagan@gmail.com>
2021-05-07 15:18:37 +0100

    set some css formatting to overall container

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,7 +3,7 @@ import React from "react";
 import styles from "../css/Container.module.css";
 
 const Container = ({ children }) => {
-  return <>{children}</>;
+  return <div className={styles.container}>{children}</div>;
 };
 
 export default Container;
