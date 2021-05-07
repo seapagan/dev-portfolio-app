36a8c441c9e8e6d19f8cd2207cf910c579978289
Grant Ramsay <seapagan@gmail.com>
2021-05-07 19:27:58 +0100

    Start on header Links and styling for CV
    This is a placeholder for the final styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,29 @@
 import React from "react";
+import { NavLink, Link } from "react-router-dom";
 
 import styles from "../css/Header.module.css";
 
 const Header = ({ settings }) => {
+  console.log(settings);
   return (
     <header className={styles.header}>
-      <div className={styles.namebadge}>&lt; {settings.name} /&gt;</div>
+      <Link to="/" className={styles.namebadge}>
+        &lt; {settings.name} /&gt;
+      </Link>
       <nav className={styles.nav}>
         <ul>
           <li>About</li>
           <li>Skills</li>
           <li>Experience</li>
           <li>Open-Source</li>
-          <li>CV</li>
+          <li>
+            <NavLink
+              activeClassName={styles.navLinkActive}
+              className={styles.navLink}
+              to="/cv">
+              CV
+            </NavLink>
+          </li>
           <li>Contact</li>
         </ul>
       </nav>
