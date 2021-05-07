a950bd85fca667888712b7960a83f429fff12378
Grant Ramsay <seapagan@gmail.com>
2021-05-07 17:14:33 +0100

    very basic footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,15 @@ import React from "react";
 import styles from "../css/Footer.module.css";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <div>
+        <div className={styles.copyright}>
+          Created with 'Developer Portfolio', &copy;Seapagan 2021
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
