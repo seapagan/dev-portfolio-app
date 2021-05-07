44dc1d498696ae3537411db969a984c6948bb036
Grant Ramsay <seapagan@gmail.com>
2021-05-07 15:12:57 +0100

    Add settings module template

---
+++ src/configure/setings-template.js
@@ -0,0 +1,6 @@
+// rename or copy this to 'settings.js' in the same folder, and change the
+// values to suit your own details
+
+export const settings = {
+  name: "Master Develpoer", // your name
+};
