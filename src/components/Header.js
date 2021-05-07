1ed5a6546ef6607a5a0fbf4ef50f4bc20d42ee0f
Grant Ramsay <seapagan@gmail.com>
2021-05-07 16:44:38 +0100

    First Styling on the Header and menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,22 @@ import React from "react";
 
 import styles from "../css/Header.module.css";
 
-const Header = () => {
-  return <header>Header</header>;
+const Header = ({ settings }) => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.namebadge}>&lt; {settings.name} /&gt;</div>
+      <nav className={styles.nav}>
+        <ul>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Experience</li>
+          <li>Open-Source</li>
+          <li>CV</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
