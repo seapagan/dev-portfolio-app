a14c0dcfa91391d5001113be7367449c2db5babd
Grant Ramsay <seapagan@gmail.com>
2021-05-07 21:52:34 +0100

    More dummy template data

--- src/configure/setings-template.js
+++ src/configure/setings-template.js
@@ -26,4 +26,50 @@ export const settings = {
       desc: "Blah blah blah about this skill",
     },
   ],
+  about: {
+    // change to link to your social medial accounts. If blank, they wont
+    // be mentioned.
+    socialLinkedin: "",
+    socialFacebook: "",
+    socialTwitter: "",
+    socialTiktok: "",
+    socialSnapchat: "",
+    socialYoutube: "",
+  },
+  experience: [
+    {
+      title: "Top Notch Developer",
+      dates: "2014 to now",
+      company: "A big Web development company",
+      link: "https://bigwebdevcorp.com",
+      responsibilities: [
+        {
+          name: "I did this",
+          desc: "This was actually what I did, and how good I was at it!",
+        },
+        {
+          name: "I also did this cool thing",
+          desc: "You won't believe how good I was at this thing",
+        },
+      ],
+    },
+    {
+      title: "Crap Developer",
+      dates: "2010 to 2014",
+      company: "A back street Web development company",
+      link: "https://coyboy-web-design.com",
+      responsibilities: [
+        {
+          name: "Tea Maker",
+          desc: "I was really bad at this and put the milk in FIRST.",
+        },
+        {
+          name: "Garbage Collector",
+          desc:
+            "Javascript does this automatically so I didn't really do much. " +
+            "However, they paid me in Chicken Nuggets so RESULT!",
+        },
+      ],
+    },
+  ],
 };
