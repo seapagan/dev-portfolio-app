35be269f9cd5b246ff9b380787c8d444be8ae817
Grant Ramsay <seapagan@gmail.com>
2021-05-08 11:39:51 +0100

    update CardWrapper component
    I will now allow custom styles to be passed down to the children

--- src/components/CardWrapper.js
+++ src/components/CardWrapper.js
@@ -2,8 +2,8 @@ import React from "react";
 
 import styles from "../css/CardWrapper.module.css";
 
-const CardWrapper = ({ children }) => {
-  return <div className={styles.wrapper}>{children}</div>;
+const CardWrapper = ({ children, className }) => {
+  return <div className={`${styles.wrapper} ${className}`}>{children}</div>;
 };
 
 export default CardWrapper;
