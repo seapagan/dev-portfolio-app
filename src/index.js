23f2b8217f8f596b3c311f16c9008de88829ef6a
Grant Ramsay <seapagan@gmail.com>
2021-05-08 11:11:19 +0100

    Set up Apollo to allow GraphQL access (GutHub)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import {
+  ApolloClient,
+  ApolloLink,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+
 // import the css reset rules
 import "modern-css-reset";
 
 import App from "./App";
 import { settings } from "./configure/settings";
 
+const httpLink = createHttpLink({
+  uri: "https://api.github.com/graphql",
+});
+
+const authLink = new ApolloLink((operation, forward) => {
+  // Retrieve the authorization token from settings.
+  const token = settings.githubToken;
+
+  // Use the setContext method to set the HTTP headers.
+  operation.setContext({
+    headers: {
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  });
+
+  // Call the next link in the middleware chain.
+  return forward(operation);
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
       <App settings={settings} />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
