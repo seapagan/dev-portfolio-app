c607d37a6637db50035ff87af30b5a4df7c5948b
Grant Ramsay <seapagan@gmail.com>
2021-05-08 14:32:51 +0100

    add commits, stars and forks details to each repo
    each has an icon from the official GitHub Octicon library

--- src/components/RepoItem.js
+++ src/components/RepoItem.js
@@ -1,12 +1,39 @@
 import React from "react";
 
+import { GitForkIcon, StarIcon, GitCommitIcon } from "@primer/octicons-react";
+
 import styles from "../css/RepoItem.module.css";
 import CardWrapper from "./CardWrapper";
 
 const RepoItem = ({ repo }) => {
+  const totalCommits = repo.defaultBranchRef.target.history.totalCount;
+  const defaultBranch = repo.defaultBranchRef.name;
+
   return (
-    <CardWrapper>
-      <div className={styles.repoItem}>{repo.name}</div>
+    <CardWrapper className={styles.repoItem}>
+      <div className={styles.name}>{repo.name}</div>
+      <div className={styles.desc}>{repo.description}</div>
+      <div className={styles.metrics}>
+        <div title={`Number of Commits on the main branch '${defaultBranch}'`}>
+          <GitCommitIcon verticalAlign="middle" size="small" />
+          <span className={styles.metricItem}>{totalCommits}</span>
+        </div>
+        <div title={`Number of Stars`}>
+          <StarIcon verticalAlign="middle" size="small" />
+          <span className={styles.metricItem}>{repo.stargazerCount}</span>
+        </div>
+        <div title={`Number of Forks`}>
+          <GitForkIcon verticalAlign="middle" size="small" />
+          <span className={styles.metricItem}>{repo.forkCount}</span>
+        </div>
+      </div>
+      <a
+        className={styles.url}
+        href={repo.url}
+        target="_blank"
+        rel="noopener noreferrer">
+        Visit on GitHub
+      </a>
     </CardWrapper>
   );
 };
