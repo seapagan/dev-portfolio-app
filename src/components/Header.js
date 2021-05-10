c87830ca3558c3bfb60541e0c5cdfbd5c857387e
Grant Ramsay <seapagan@gmail.com>
2021-05-10 21:35:08 +0100

    implement CSS responsive menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,79 +1,102 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import { Link } from "react-scroll";
 
 import styles from "../css/Header.module.css";
 
 const Header = ({ name }) => {
+  const toggleRef = useRef();
+
+  const closeMenu = () => {
+    if (toggleRef.current.checked) {
+      toggleRef.current.checked = false;
+    }
+  };
+
   return (
-    <header className={styles.header}>
-      {/* <input type="checkbox" className="mobileMenuButton"></input> */}
-      <Link to="/" className={styles.namebadge}>
-        &lt; {name} /&gt;
-      </Link>
-      <nav className={styles.nav}>
-        <ul>
-          <li>
-            <Link
-              className={styles.navLink}
-              to="about-section"
-              spy={true}
-              smooth={true}
-              duration={1000}>
-              About
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={styles.navLink}
-              to="skills-section"
-              spy={true}
-              smooth={true}
-              duration={1000}>
-              Skills
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={styles.navLink}
-              to="experience-section"
-              spy={true}
-              smooth={true}
-              duration={1000}>
-              Experience
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={styles.navLink}
-              to="openSource-section"
-              spy={true}
-              smooth={true}
-              duration={1000}>
-              Open-Source
-            </Link>
-          </li>
-          {/* <li>
-              <NavLink
-                activeClassName={styles.navLinkActive}
+    <>
+      <header className={styles.header}>
+        <Link to="/" className={styles.namebadge}>
+          &lt; {name} /&gt;
+        </Link>
+        <input
+          ref={toggleRef}
+          type="checkbox"
+          id="nav-toggle"
+          className={styles.navToggle}
+        />
+        <label htmlFor="nav-toggle" className={styles.navToggleLabel}>
+          <span></span>
+        </label>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link
+                onClick={closeMenu}
                 className={styles.navLink}
-                to="/cv">
-                CV
-              </NavLink>
-            </li> */}
-          <li>
-            <Link
-              className={styles.navLink}
-              to="contact-section"
-              spy={true}
-              smooth={true}
-              duration={1000}>
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+                to="about-section"
+                spy={true}
+                smooth={true}
+                duration={1000}>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={closeMenu}
+                className={styles.navLink}
+                to="skills-section"
+                spy={true}
+                smooth={true}
+                duration={1000}>
+                Skills
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={closeMenu}
+                className={styles.navLink}
+                to="experience-section"
+                spy={true}
+                smooth={true}
+                duration={1000}>
+                Experience
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={closeMenu}
+                className={styles.navLink}
+                to="openSource-section"
+                spy={true}
+                smooth={true}
+                duration={1000}>
+                Open-Source
+              </Link>
+            </li>
+            {/* <li>
+                <NavLink
+                  activeClassName={styles.navLinkActive}
+                  className={styles.navLink}
+                  to="/cv">
+                  CV
+                </NavLink>
+              </li> */}
+            <li>
+              <Link
+                onClick={closeMenu}
+                className={styles.navLink}
+                to="contact-section"
+                spy={true}
+                smooth={true}
+                duration={1000}>
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 };
 
