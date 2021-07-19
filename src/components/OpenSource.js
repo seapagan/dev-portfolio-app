b2be77ed294c9b9667ef81db6ba09e29de693cbd
Grant Ramsay <seapagan@gmail.com>
2021-07-19 16:49:12 +0100

    Rename div to h2 in open-source section title

--- src/components/OpenSource.js
+++ src/components/OpenSource.js
@@ -7,7 +7,7 @@ import OpenSourceUser from "./OpenSourceUser";
 const OpenSource = ({ usernames }) => {
   return (
     <section id="openSource-section">
-      <div className="section__title">OpenSource Projects</div>
+      <h2 className="section__title">OpenSource Projects</h2>
       {[].concat(usernames).map((username, index) => {
         return (
           <Wrapper key={index} className={styles.wrapper}>
