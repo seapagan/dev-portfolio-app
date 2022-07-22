cef25c80732e2d040fd28f8dd77f7a763d22d1b5
Grant Ramsay <seapagan@gmail.com>
2022-07-22 14:22:38 +0100

    fix error d/t 'react-loader-spinner' upgrade

--- src/components/RepoLoading.js
+++ src/components/RepoLoading.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Loader from "react-loader-spinner";
+import { TailSpin } from "react-loader-spinner";
 
 import styles from "../css/RepoLoading.module.css";
 
 const Loading = () => {
   return (
     <div className={styles.loading}>
-      <Loader type="TailSpin" color="gray" height={50} width={50} />
+      <TailSpin color="gray" height={50} width={50} />
       <p>Querying GitHub for Repositories...</p>
     </div>
   );
