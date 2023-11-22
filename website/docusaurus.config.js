1130e76572e545724ce3c988e260064f99f48fb3
Grant Ramsay <seapagan@gmail.com>
2023-11-22 17:30:33 +0000

    add a CNAME for the website

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://seapagan.github.io",
+  url: "https://portfolio.seapagan.net",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/dev-portfolio-app/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
