26efea0d77dfab2f0b1b78baa94bae2dbe6b764c
Grant Ramsay <seapagan@gmail.com>
2023-12-05 17:38:05 +0000

    take GA id from .env file

--- vite.config.js
+++ vite.config.js
@@ -2,44 +2,48 @@
 import react from "@vitejs/plugin-react";
 
 import { visualizer } from "rollup-plugin-visualizer";
-import { defineConfig, splitVendorChunkPlugin } from "vite";
+import { defineConfig, loadEnv, splitVendorChunkPlugin } from "vite";
 import eslint from "vite-plugin-eslint";
 import { ViteMinifyPlugin } from "vite-plugin-minify";
 import { VitePluginRadar } from "vite-plugin-radar";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    svgr(),
-    VitePluginRadar({
-      analytics: {
-        id: "", // Your Google Analytics tracking ID (V4) qeg. G-XXXXXXXXXX
-        config: {
-          cookie_flags: "SameSite=None;Secure",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      react(),
+      svgr(),
+      VitePluginRadar({
+        analytics: {
+          id: `${env.VITE_GA_ID ?? ""}`,
+          config: {
+            cookie_flags: "SameSite=None;Secure",
+          },
         },
-      },
-    }),
-    eslint(),
-    splitVendorChunkPlugin(),
-    ViteMinifyPlugin({}),
-    visualizer(),
-  ],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("react-github-readme-button")) {
-            return "github-button";
-          }
-          if (id.includes("highlight.js")) {
-            return "highlight.js";
-          }
-          if (id.includes("@apollo")) {
-            return "@apollo";
-          }
+      }),
+      eslint(),
+      splitVendorChunkPlugin(),
+      ViteMinifyPlugin({}),
+      visualizer(),
+    ],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes("react-github-readme-button")) {
+              return "github-button";
+            }
+            if (id.includes("highlight.js")) {
+              return "highlight.js";
+            }
+            if (id.includes("@apollo")) {
+              return "@apollo";
+            }
+          },
         },
       },
     },
-  },
+  };
 });
